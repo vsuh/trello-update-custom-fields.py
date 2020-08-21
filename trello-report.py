0fc31e5702972f12cb0cd1b395c557df18a1bd80
vsuh <cyx@bk.ru>
2020-08-21 15:29:47 +0300

    wip-1

--- trello-report.py
+++ trello-report.py
@@ -21,6 +21,7 @@ parser = argparse.ArgumentParser(add_help=True , description='Формирова
 REPFILE = "report.csv"
 rigth_names = ['d', 'cd', 'pd', 'w', 'cw', 'pw', 'm', 'cm', 'pm', 'y', 'cy', 'py']
 margins = {'name':None, 'past':None, 'beg':None, 'end':None, 'date': None}
+cust_fields_names = ['id', 'Автор', '≡']
 
 def init():
     c_handler = logging.StreamHandler()
@@ -93,10 +94,115 @@ def set_period_by_name():
         margins['end'] = beg + rd(years=1) - rd(seconds=1)
     log.debug(f"вычисленный период: c {margins['beg'].strftime('%d-%m-%y')} по {margins['end'].strftime('%d-%m-%y %H:%M:%S')}")
 
+def get_board_by_name(name):
+    """
+    возвращает описание (словарь) доски по её имени
+    """
+    log.debug(f"Ищем доску с именем `{name}`")
+    for brd in api.organizations.get_board(idOrg_or_name=ORGANISATION, filter='open'):
+        if name.upper() == brd['name'].upper():
+            log.info(f"Нашли доску `{brd['name']}`(id={brd['id']})")
+            return brd
+    return None
+
+def cardDate(card):
+        creation_time = dt.fromtimestamp(int(card['id'][0:8],16))
+        msk = pytz.timezone('Europe/Moscow')
+        log.debug(f'Получили дату карточки № {card["idShort"]}: {creation_time.strftime("%d-%m-%y %H:%M:%S")}')
+        return msk.localize(creation_time)
+
+def get_c_field(brd, name):
+    """
+    возвращает описание (словарь) пользовательского поля по его имени
+    """
+    import requests
+    import json
+    log.debug(f"Ищем в свойствах доски `{brd['name']}` Custom field с именем `{name}`")
+    id = brd['id']
+    url = f'https://api.trello.com/1/boards/{id}/customFields'
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    query = {
+    'key': APIKY,
+    'token': TOKEN
+    }
+
+    response = requests.request(
+    "GET",
+    url,
+    headers=headers,
+    params=query
+    )
+    log.debug(f"Получаем все польз. поля на доске `{brd['name']}`")
+    flds = json.loads(response.text)
+    for ff in flds:
+        if ff['name'].upper() == name.upper():
+            log.info(f"Нашли поле `{ff['name']}`(id={ff['id']}) на доске `{brd['name']}`")
+            return ff
+    return None
+
+def get_custom_fields(board):
+    """
+    возвращает словарь словарей с идентификаторами custom fields из доски board
+    """
+    ret = {}
+    for cf_name in cust_fields_names:
+        cf = get_c_field(board, cf_name)
+        ret.update({cf_name: {
+             'brd': board['name']
+            ,'br_id': board['id']
+            ,'id': cf['id']
+            }
+        })
+    return ret
+
+
+def get_custom_field_value(card_id,custom_field_id):
+    """
+    получает значение custom field из карточки
+    """
+    import requests
+    url = f'https://api.trello.com/1/card/{card_id}/customField/{custom_field_id}/item?key={APIKY}&token={TOKEN}'
+    
+    payload = {'token':TOKEN,'key':APIKY}
+    request = requests.get(url,json=payload) 
+    return request
+
+
+def get_cards_in_period():
+    """
+    возвращает список карточек удовлетворяющих условию
+    """
+    boards = ['*Текучка*', 'Архив']
+    cards = []
+    for name in boards:
+        brd = get_board_by_name(name)
+        custom_fields = get_custom_fields(brd)
+
+        for crd in api.boards.get_card_filter(board_id=brd['id'], filter='open'):
+            crddate = cardDate(crd)
+            if crddate > margins['end'] or crddate < margins['beg']:
+                continue
+            card = {
+                 'date': crddate
+                ,'name': crd['name']
+                ,'desc': crd['desc'] if len(crd['desc'])<94 else crd['desc'][:90]+'...'
+                ,'bord': 'name'
+            }
+            for cf_name, dict_cf in custom_fields.items():
+                cf_value = get_custom_field_value(crd['id'], dict_cf['id'])
+                card.update( {dict_cf: cf_value} )
+            cards.append(card)
+        return cards
+
 def report():
     set_period_by_name()
-    # pp(margins)
-    pass
+    cards_list = get_cards_in_period()
+    pp(cards_list)
+    
 
 init()
 report()
